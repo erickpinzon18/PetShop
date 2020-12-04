5a9c23456252f9acf1f14eb9bd57c26e694f9522
Erick Pinzon <erickpinzonhuerta@gmail.com>
2020-12-04 09:51:44 -0600

    Add Body

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Import Header
+
+import { HeaderComponent } from './components/header/header.component';
+
+// Import Body
+
+import { BodyComponent } from './components/body/body.components';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
